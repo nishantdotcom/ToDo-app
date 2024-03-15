0c2074804e73b396073dcf600f365bbee3fc8920
nishantdotcom <nishantsingh1308@gmail.com>
2024-03-15 22:03:26 +0530

    Update todo

--- src/component/TaskInput.tsx
+++ src/component/TaskInput.tsx
@@ -1,36 +1,98 @@
 import { useState } from "react";
 
+type TodoType = {
+  title: string;
+  id: number;
+  desc: string;
+};
+
 function TaskInput() {
   const [task, setTask] = useState<string>("");
-  const [list, setlist] = useState<string[]>([]);
+  const [taskDesc, setTaskDescription] = useState<string>("");
+  const [list, setlist] = useState<TodoType[]>([]);
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setTask(event.target.value);
+  }
+  function handleDescription(event: React.ChangeEvent<HTMLInputElement>) {
+    setTaskDescription(event.target.value);
+  }
+  function handleRadioEvent(event: React.MouseEvent<HTMLInputElement>) {
+    console.log(event);
   }
 
   function handleSubmit() {
     setTask("");
-    const getTask: string[] = list;
-    getTask.push(task);
+    setTaskDescription("");
+    const getTask: TodoType[] = list;
+    const task_id: number = list.length;
+    getTask.push({
+      title: task,
+      desc: taskDesc,
+      id: task_id,
+    });
+    console.log(getTask);
     setlist(getTask);
   }
 
   return (
-    <div className="flex justify-center p-2 gap-x-4">
-      <div>Enter the task you want to add:</div>
-      <div>
+    <div className="p-2 gap-x-4">
+      <div className="flex justify-center text-2xl font-bold p-4">
+        Enter the task you want to add
+      </div>
+      <div className="flex justify-center ">
         <input
           type="text"
           value={task}
           placeholder="Enter task"
           onChange={(event) => handleChange(event)}
-          className="border border-black rounded-lg"
+          className="border border-black rounded-lg h-16 w-96 p-6"
         />
-        <button onClick={handleSubmit}>Submit</button>
       </div>
-      <div>Task List</div>
-      {list.map((data: string) => {
-        return <div className="text-red-600">{data}</div>;
-      })}
+      <div className="flex justify-center text-2xl font-bold p-4">
+        Enter Task Description
+      </div>
+      <div className="flex justify-center">
+        <input
+          type="text"
+          value={taskDesc}
+          placeholder="Enter description"
+          className="border border-black rounded-lg h-16 w-96 p-4"
+          onChange={(event) => {
+            handleDescription(event);
+          }}
+        />
+      </div>
+      <div className=" flex justify-center pt-8">
+        <button
+          onClick={handleSubmit}
+          className="p-4 text-white font-semibold bg-red-300 rounded-2xl"
+        >
+          Submit
+        </button>
+      </div>
+
+      <div className="flex justify-center p-4 text-xl">Task List</div>
+      <div>
+        {list.map((data: TodoType) => {
+          return (
+            <div
+              className="flex justify-start p-4 border-2 bg-slate-100 m-2 text-violet-500 font-medium text-2xl rounded-lg gap-x-10 "
+              key={data.id}
+            >
+              <div>
+                <input
+                  type="radio"
+                  onClick={(event) => {
+                    handleRadioEvent(event);
+                  }}
+                />
+              </div>
+              <div>{data.title}</div>
+              <div>{data.desc}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
